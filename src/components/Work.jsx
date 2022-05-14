85012078028dcd29c978942216b324d86828838a
Heidelol <heideloliver@gmail.com>
2022-05-13 22:50:24 -0500

    segundo proyecto

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GithubApp from '../assets/gitapp.png'
+import GifApp from '../assets/gifapp.png'
 import WorkImg from '../assets/workImg.jpeg';
 import realEstate from '../assets/realestate.jpg';
 
@@ -42,12 +43,47 @@ const Work = () => {
               </div>
             </div>
 
-            
-           
-            
-            
-            
-            
+
+            <div
+              style={{ backgroundImage: `url(${GifApp})` }}
+              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            >
+              {/* Hover Effects */}
+              <div className='opacity-0 group-hover:opacity-100'>
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                  React JS Application
+                </span>
+                <div className='pt-8 text-center'>
+                  <a target="_blank" href='https://gif-app-exp.vercel.app/'>
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                      Demo
+                    </button>
+                  </a>
+                  <a target="_blank" href='https://github.com/Heidelol/GifAppExp'>
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           </div>
         </div>
       </div>
